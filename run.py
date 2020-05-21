00ba70fff9776d1141bdace93b210affa48900f1
iii <cclan8@outlook.com>
2020-05-21 23:17:39 +0800

    实时检测动作

--- run.py
+++ run.py
@@ -1,13 +1,14 @@
-import argparse
+import argparse, time, os
 import cv2 as cv
 import mxnet as mx
-import time
+import numpy as np
 import gluoncv as gcv
 from gluoncv import data
 from gluoncv.data import mscoco
 from gluoncv.model_zoo import get_model
 from gluoncv.data.transforms.pose import detector_to_simple_pose, heatmap_to_coord
 from gluoncv.utils.viz import cv_plot_image, cv_plot_keypoints
+from joblib import load
 
 # 读取参数
 parser = argparse.ArgumentParser()
@@ -19,7 +20,6 @@ fps_time = 0
 # 设置模型
 ctx = mx.gpu()
 
-## 目标检测
 detector_name = "ssd_512_mobilenet1.0_coco"
 detector = get_model(detector_name, pretrained=True, ctx=ctx)
 
@@ -30,6 +30,10 @@ detector.reset_class(classes=['person'], reuse_weights={'person':'person'})
 
 detector.hybridize()
 estimator.hybridize()
+
+le = load(os.path.join('model', 'label.joblib'))
+pca = load(os.path.join('model', 'pca.joblib'))
+cf = load(os.path.join('model', 'classifier.joblib'))
 
 # 视频读取
 cap = cv.VideoCapture(args.video)
@@ -51,13 +55,20 @@ while ret:
         pred_coords, confidence = heatmap_to_coord(predicted_heatmap, upscale_bbox)
         img = cv_plot_keypoints(img, pred_coords, confidence, class_IDs, bounding_boxs, scores)
 
+        # 动作识别
+        X = predicted_heatmap.asnumpy().flatten().reshape((len(upscale_bbox), -1))
+        X = pca.transform(X)
+        action = le.inverse_transform(cf.predict(X))
 
-    cv_plot_image(img, upperleft_txt=f"FPS:{(1.0 / (time.time() - fps_time)):.2f}", upperleft_txt_corner=(10, 15))
+
+    cv_plot_image(img, 
+        upperleft_txt=f"FPS:{(1.0 / (time.time() - fps_time)):.2f}", upperleft_txt_corner=(10,25),
+        left_txt_list=action)
     fps_time = time.time()
     
     # ESC键退出
     if cv.waitKey(1) == 27:
-            break
+        break
 
     ret, frame = cap.read()
 
